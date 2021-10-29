a31ca814abddac1c48b221f660f01440dee141db
Allen Terescenco <allenterescencobq@gmail.com>
2021-10-29 15:19:17 +0100

    Added routing and live data from API.

--- src/stories/sampleData.js
+++ src/stories/sampleData.js
@@ -71,7 +71,7 @@ export default {
             name: "English",
         },
     ],
-    status: "Releasedd",
+    status: "Released",
     tagline: "Darkness rises... and light to meet it",
     title: "Star Wars: The Last Jedi",
     video: false,
